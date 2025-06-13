e543752e41ec453e5cb3d48f7545b98224a0314f
Ali Raza Syed <a.raza.syed.123@gmail.com>
2025-06-13 09:35:15 -0400

    Upgraded @rollup and relevant plugins.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,8 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
-import pkg from './package.json';
+//import pkg from './package.json';
+import pkg from './package.json' assert { type: 'json' };
 
 const input = 'src/index.js';
 const external = Object.keys(pkg.dependencies || {});
