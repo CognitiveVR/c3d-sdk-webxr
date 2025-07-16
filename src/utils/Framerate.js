741658a21cf32c1668c2a8a6317474eb36ed1d9b
Ali Raza Syed <a.raza.syed.123@gmail.com>
2025-07-16 19:41:01 -0400

    A new method for calculating average fps that takes into consideration frame-time (time between frames).

--- src/utils/Framerate.js
+++ src/utils/Framerate.js
@@ -1,39 +1,58 @@
-// All web-based engines rely on requestAnimateFrame
-// requestAnimateFrame tries to synchronize the framerate to the monitors ~ 60fps 
-
 import { isBrowser } from "./environment";
 
 class FPSTracker {
   constructor() {
-    this.frames = 0; // simple counter, +1 whenever browser draws a new frame 
-    this.intervalId = null; // associated id with the 1 second timer 
-    this.animationFrameId = null; // associated id with each frame 
-    this._loop = this._loop.bind(this); 
+    this.animationFrameId = null;
+    this.lastTime = 0;
+    this.frameCount = 0;
+    this.elapsedTime = 0;
+    this.samplePeriod = 1000; // 1 second
+    this._loop = this._loop.bind(this);
   }
-  _loop() {
-    this.frames++;
-    this.animationFrameId = requestAnimationFrame(this._loop);
-  }
-  start(callback) {
-    if(!isBrowser || this.intervalId){ // requestAnimationFrame only available on browsers 
-      return
-    }
-    this.frames = 0;
-    this._loop();
 
-    this.intervalId = setInterval(() => {
-      callback(this.frames); // Report the number of frames rendered in the last second.
-      this.frames = 0; // Reset the counter for the next second.
-    }, 1000);     // 1 second interval to report 
+  /**
+   * _loop runs on every animation frame, calculates the delta time since the last frame and reports the FPS
+   * once the sample period has been reached.
+   * @param {number} currentTime - Timestamp provided by requestAnimationFrame.
+   * @param {function(number): void} callback - The function to call with the FPS value.
+   */
+  _loop(currentTime, callback) { 
+    if (this.lastTime > 0) {
+      const deltaTime = currentTime - this.lastTime;
+      this.elapsedTime += deltaTime;
+      this.frameCount++;
+
+      if (this.elapsedTime >= this.samplePeriod) {  // Check if the sample period (1 sec) has been reached
+        const averageFps = (this.frameCount / this.elapsedTime) * 1000; // Calculate the average FPS over the sample period
+        callback(averageFps);
+
+        // Reset for the next sample period
+        this.frameCount = 0;
+        this.elapsedTime = 0;
+      }
+    }
+
+    this.lastTime = currentTime;
+    this.animationFrameId = requestAnimationFrame((time) => this._loop(time, callback));
   }
-  stop() {
-    if (!isBrowser) {
+
+  /**
+   * Starts the FPS tracking.
+   * @param {function(number): void} callback - The function to call with the FPS value (RecordSensor).
+   */
+  start(callback) {
+    if (!isBrowser || this.animationFrameId) {
       return;
     }
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-      this.intervalId = null;
-    }
+    
+    this.lastTime = 0;
+    this.frameCount = 0;
+    this.elapsedTime = 0;
+    
+    this.animationFrameId = requestAnimationFrame((time) => this._loop(time, callback));
+  }
+
+  stop() {
     if (this.animationFrameId) {
       cancelAnimationFrame(this.animationFrameId);
       this.animationFrameId = null;
